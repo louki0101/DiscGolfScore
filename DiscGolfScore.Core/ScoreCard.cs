8bbaea04d3ad97687d36e8c3abcef73d079031f2
Michael Thelen <michael.thelen@orau.org>
2022-05-30 15:29:29 -0700

    Add project files.

---
+++ DiscGolfScore.Core/ScoreCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiscGolfScore.Core
+{
+    public class ScoreCard
+    {
+        public int Id { get; set; }
+
+        public string Location { get; set; }
+
+        public int Holes { get; set; }
+
+        public Dictionary<int, int> Scores { get; set; } 
+    }
+}
